d952de66088aae71ce5f23b1bb93e0d5fa3d453e
alloyking1 <alloyking1@gmail.com>
2025-11-13 10:33:42 +0100

    modified contact us form to send email

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Inertia\Inertia;
+use Illuminate\Support\Facades\Mail;
 
 class PagesController extends Controller
 {
@@ -25,5 +26,29 @@ class PagesController extends Controller
     public function contact()
     {
         return Inertia::render('Contact');
+    }
+
+    public function sendContactForm(Request $request)
+    {
+        $validated = $request->validate([
+            // 'name' => 'required|string|max:255',
+            'email' => 'required|email|max:255',
+            'subject' => 'required|string|max:255',
+            'message' => 'required|string',
+        ]);
+
+        try {
+            $email = env('MAIL_FROM_ADDRESS');
+            $body = 'Sent from: ' . $request->email . "\n" . 'Body: ' . $request->message . "\n\n" ;
+
+            Mail::raw($body, function ($message) use ($email, $subject) {
+                $message->to($email)
+                        ->subject($request->subject);
+            });
+        } catch (\Exception $e) {
+            return redirect()->back()->with('error', 'An error occurred while sending the email.');
+        }
+
+        return redirect()->back()->with('success', 'Your message has been sent successfully!');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -8,6 +8,7 @@ Route::get('/', [App\Http\Controllers\PagesController::class, 'home'])->name('ho
 Route::get('/about', [App\Http\Controllers\PagesController::class, 'about'])->name('about');
 Route::get('/contact', [App\Http\Controllers\PagesController::class, 'contact'])->name('contact');
 Route::get('/service', [App\Http\Controllers\PagesController::class, 'service'])->name('service');
+Route::post('/send-contact-form', [App\Http\Controllers\PagesController::class, 'sendContactForm'])->name('send-contact-form');
 
 Route::get('dashboard', function () {
     return Inertia::render('Dashboard');
